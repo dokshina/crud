7ee99f831f268c0e7d7bd5040697a4c436c3dc07
Oleg Babin <babinoleg@mail.ru>
2020-10-02 19:41:33 +0300

    select: first dirty merger integraion attempt
    In scope of #33

--- crud/select/iterator.lua
+++ crud/select/iterator.lua
@@ -1,196 +1,120 @@
-local errors = require('errors')
-local fiber = require('fiber')
+local buffer = require('buffer')
+local msgpack = require('msgpack')
+local key_def_lib = require('key_def')
+local merger = require('merger')
 
-local dev_checks = require('crud.common.dev_checks')
-local utils = require('crud.common.utils')
+local SELECT_FUNC_NAME = '__select'
+local CALL_FUNC_NAME = '__call'
 
-local UpdateTuplesError = errors.new_class('UpdateTuplesError')
-local GetTupleError = errors.new_class('GetTupleError')
+-- TODO: implement key_def cache
+local function get_key_def(replicasets, space_name, index_name)
+    -- Get requested and primary index metainfo.
+    local conn = select(2, next(replicasets)).master.conn
+    local primary_index = conn.space[space_name].index[0]
+    local index = conn.space[space_name].index[index_name]
 
-local Heap = require('crud.common.heap')
+    -- Create a key def.
+    local key_def = key_def_lib.new(index.parts)
+    if not index.unique then
+        key_def = key_def:merge(key_def_lib.new(primary_index.parts))
+    end
 
-local Iterator = {}
-Iterator.__index = Iterator
-
-function Iterator.new(opts)
-    dev_checks({
-        space_name = 'string',
-        space_format = 'table',
-        comparator = 'function',
-        iteration_func = 'function',
-
-        plan = 'table',
-
-        batch_size = 'number',
-        replicasets = 'table',
-
-        timeout = '?number',
-    })
-
-    local iter = {
-        space_name = opts.space_name,
-        space_format = opts.space_format,
-        iteration_func = opts.iteration_func,
-
-        plan = opts.plan,
-
-        timeout = opts.timeout,
-
-        replicasets = table.copy(opts.replicasets),
-        replicasets_count = utils.table_count(opts.replicasets),
-        empty_replicasets = {},
-        empty_replicasets_count = 0,
-
-        batch_size = opts.batch_size,
-
-        tuples_by_replicasets = {},
-        next_tuple_indexes = {},
-
-        heap = Heap.new({ comparator = opts.comparator }),
-        tuples_count = 0,
-
-        update_tuples_channel = fiber.channel(1),
-        wait_for_update = false,
-    }
-
-    setmetatable(iter, Iterator)
-
-    iter:_update_replicasets_tuples(iter.plan.after_tuple)
-
-    return iter
+    return key_def
 end
 
-function Iterator:has_next()
-    if self.heap:size() == 0 and self.empty_replicasets_count >= self.replicasets_count then
-        return false
-    end
+local function decode_metainfo(buf)
+    -- Skip an array around a call return values.
+    local len
+    len, buf.rpos = msgpack.decode_array_header(buf.rpos, buf:size())
+    assert(len == 2)
 
-    if self.plan.total_tuples_count ~= nil and self.tuples_count >= self.plan.total_tuples_count then
-        return false
-    end
-
-    return true
+    -- Decode a first return value (metainfo).
+    local res
+    res, buf.rpos = msgpack.decode(buf.rpos, buf:size())
+    return res
 end
 
-local function get_next_replicaset_tuple(iter, replicaset_uuid)
-    local replicaset_tuples = iter.tuples_by_replicasets[replicaset_uuid]
-    local next_tuple = replicaset_tuples[iter.next_tuple_indexes[replicaset_uuid]]
+--- Wait for a data chunk and request for the next data chunk.
+local function fetch_chunk(context, state)
+    local net_box_opts = context.net_box_opts
+    local buf = context.buffer
+    local call_args = context.call_args
+    local replicaset = context.replicaset
+    local future = state.future
 
-    iter.next_tuple_indexes[replicaset_uuid] = iter.next_tuple_indexes[replicaset_uuid] + 1
-
-    return next_tuple
-end
-
-local function update_replicasets_tuples(iter, after_tuple, replicaset_uuid)
-    local replicasets = {}
-    if replicaset_uuid == nil then
-        replicasets = iter.replicasets
-    else
-        replicasets[replicaset_uuid] = iter.replicasets[replicaset_uuid]
-    end
-
-    local limit_per_storage_call = iter.batch_size
-    if iter.total_tuples_count ~= nil then
-        limit_per_storage_call = math.min(iter.batch_size, iter.total_tuples_count - iter.tuples_count)
-    end
-
-    local results_map, err = iter.iteration_func(iter.space_name, iter.plan, {
-        after_tuple = after_tuple,
-        replicasets = replicasets,
-        timeout = iter.timeout,
-        limit = limit_per_storage_call,
-    })
-    if err ~= nil then
-        return false, UpdateTuplesError:new('Failed to select tuples from storages: %s', err)
-    end
-
-    for replicaset_uuid, tuples in pairs(results_map) do
-        if #tuples == 0 or #tuples < limit_per_storage_call then
-            iter.empty_replicasets[replicaset_uuid] = true
-            iter.empty_replicasets_count = iter.empty_replicasets_count + 1
-        end
-
-        iter.tuples_by_replicasets[replicaset_uuid] = tuples
-        iter.next_tuple_indexes[replicaset_uuid] = 1
-
-        local next_tuple = get_next_replicaset_tuple(iter, replicaset_uuid)
-
-        if next_tuple ~= nil then
-            iter.heap:add(next_tuple, {
-                replicaset_uuid = replicaset_uuid
-            })
-        end
-    end
-
-    return true
-end
-
-function Iterator:_update_replicasets_tuples(after_tuple, replicaset_uuid)
-    self.wait_for_update = true
-
-    local function _update_replicasets_tuples(channel, iter, after_tuple, replicaset_uuid)
-        local ok, err = update_replicasets_tuples(iter, after_tuple, replicaset_uuid)
-        channel:put({
-            ok = ok,
-            err = err,
-        })
-    end
-
-    fiber.create(_update_replicasets_tuples, self.update_tuples_channel, self, after_tuple, replicaset_uuid)
-end
-
-function Iterator:get()
-
-    if self.wait_for_update then
-        -- wait for _update_replicasets_tuples
-
-        self.wait_for_update = false
-
-        local res = self.update_tuples_channel:get()
-
-        if res == nil then
-            if self.update_tuples_channel:is_closed() then
-                return nil, GetTupleError:new("Channel is closed")
-            end
-
-            return nil, GetTupleError:new("Timeout was reached")
-        end
-
-        if not res.ok then
-            return nil, GetTupleError:new("Failed to get tuples from storages: %s", res.err)
-        end
-    end
-
-    local node = self.heap:pop()
-
-    if node == nil then
+    -- The source was entirely drained.
+    if future == nil then
         return nil
     end
 
-    local tuple = node.obj
-    local last_tuple_replicaset_uuid = node.meta.replicaset_uuid
-
-    self.tuples_count = self.tuples_count + 1
-
-    if self.plan.total_tuples_count == nil or self.tuples_count < self.plan.total_tuples_count then
-        local replicaset_tuples_count = #self.tuples_by_replicasets[last_tuple_replicaset_uuid]
-        local next_tuple_index = self.next_tuple_indexes[last_tuple_replicaset_uuid]
-
-        if next_tuple_index <= replicaset_tuples_count then
-            local next_tuple = get_next_replicaset_tuple(self, last_tuple_replicaset_uuid)
-
-            self.heap:add(next_tuple, {
-                replicaset_uuid = last_tuple_replicaset_uuid
-            })
-        elseif not self.empty_replicasets[last_tuple_replicaset_uuid] then
-            self:_update_replicasets_tuples(
-                tuple,
-                last_tuple_replicaset_uuid
-            )
-        end
+    -- Wait for requested data.
+    local res, err = future:wait_result()
+    if res == nil then
+        error(err)
     end
 
-    return tuple
+    -- Decode metainfo, leave data to be processed by the merger.
+    local cursor = decode_metainfo(buf)
+
+    -- Check whether we need the next call.
+    if cursor.is_end then
+        local next_state = {}
+        return next_state, buf
+    end
+
+    -- Request the next data while we processing the current ones.
+    -- Note: We reuse the same buffer for all request to a replicaset.
+    local next_call_args = call_args
+
+    -- change context.call_args too, but it does not matter
+    next_call_args[4].after_tuple = cursor.after_tuple
+    local next_future = replicaset:callro(CALL_FUNC_NAME,
+            {{func_name = SELECT_FUNC_NAME, func_args = next_call_args}},
+            net_box_opts)
+
+    local next_state = {future = next_future}
+    return next_state, buf
 end
 
-return Iterator
+local reverse_iterators = {
+    [box.index.LE] = true,
+    [box.index.LT] = true,
+    [box.index.REQ] = true,
+}
+
+local function new(replicasets, space_name, index_id, conditions, opts)
+    opts = opts or {}
+    local key_def = get_key_def(replicasets, space_name, index_id)
+    local call_args = {space_name, index_id, conditions, opts}
+
+    -- Request a first data chunk and create merger sources.
+    local merger_sources = {}
+    for _, replicaset in pairs(replicasets) do
+        -- Perform a request.
+        local buf = buffer.ibuf()
+        local net_box_opts = {is_async = true, buffer = buf, skip_header = true}
+        local future = replicaset:callro(CALL_FUNC_NAME,
+                {{func_name = SELECT_FUNC_NAME, func_args = call_args}},
+                net_box_opts)
+
+        -- Create a source.
+        local context = {
+            net_box_opts = net_box_opts,
+            buffer = buf,
+            call_args = call_args,
+            replicaset = replicaset,
+        }
+        local state = {future = future}
+        local source = merger.new_buffer_source(fetch_chunk, context, state)
+        table.insert(merger_sources, source)
+    end
+
+    local merger_inst = merger.new(key_def, merger_sources, {
+        reverse = reverse_iterators[opts.iter],
+    })
+    return merger_inst
+end
+
+return {
+    new = new,
+}
